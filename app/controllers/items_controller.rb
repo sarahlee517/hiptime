d0c6bdfafdc214f4658a44f9c64802700bc4bcff
Sarah <sarahlee517@gmail.com>
2016-11-25 11:04:11 +0800

    add edit/destroy action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,22 @@ class ItemsController < ApplicationController
 	def show
 	end
 
+	def edit
+
+	end
+
+	def update
+		if @item.update(item_params)
+			redirect_to item_path(@item)
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@item.destroy
+		redirect_to root_path
+	end
 
 	private
 
