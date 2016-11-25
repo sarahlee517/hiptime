8a0f64dba51d62ce76e78e0c4b40090ca8b0ff71
Sarah <sarahlee517@gmail.com>
2016-11-25 10:35:07 +0800

    implement show action

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+	before_action :find_item, only: [:show, :edit, :update, :destroy]
 	def index
 		@items = Item.all.order("created_at DESC")
 	end
@@ -16,10 +17,18 @@ class ItemsController < ApplicationController
 		end
 	end
 
+	def show
+	end
+
+
 	private
 
 	def item_params
 		params.require(:item).permit(:title, :description)
+	end
+
+	def find_item
+		@item = Item.find(params[:id])
 	end
 
 end
